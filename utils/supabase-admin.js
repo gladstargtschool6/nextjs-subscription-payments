0154661d243fd2f09267e5b3e4cf6af515661aaa
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-03 09:01:53 +0100

    Update supabase-admin.js

--- utils/supabase-admin.js
+++ utils/supabase-admin.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseAdmin = createClient(
-  process.env.https://imdvvfcbtrfewysxgrnr.supabase.co,
-  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNDMzNzg3MSwiZXhwIjoxOTI5OTEzODcxfQ.O7Ck0RFbXxsJ9BpxdAuey5O_MJcC_lHM06xDYKTbWxg
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export const getUser = async (token) => {
