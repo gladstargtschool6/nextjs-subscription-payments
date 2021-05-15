548d3c4724c13e2820c96040674f8e709fb7ef4e
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-15 14:31:21 +0100

    Update supabase-client.js

--- utils/supabase-client.js
+++ utils/supabase-client.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   process.env.imdvvfcbtrfewysxgrnr.supabase.co,
-  process.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNDMzNzg3MSwiZXhwIjoxOTI5OTEzODcxfQ.O7Ck0RFbXxsJ9BpxdAuey5O_MJcC_lHM06xDYKTbWxg
 );
 
 export const getActiveProductsWithPrices = async () => {
